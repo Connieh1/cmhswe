2ebf4a17a6f60748114b9bab7dfe25b14dc2fe25
Connie Hyman <connie2630a@gmail.com>
2021-07-01 12:17:03 -0700

    Prepare files for deployment

---
+++ dist/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $("#slides").superslides({
+    animation: "fade",
+    play: 4000,
+    pagination: false,
+  });
+
+  const typed = new Typed(".typed", {
+    strings: ["Software Engineer", "Web Developer"],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 2000,
+    showCursor: false,
+  });
+
+  Fancybox.bind("[data-fancybox]");
+
+  //$("[data-fancybox]").fancybox();
+
+  $(".items").isotope({
+    filter: "*",
+    animationOptions: {
+      duration: 1500,
+      easing: "linear",
+      queue: false,
+    },
+  });
+});
